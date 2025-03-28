97d862ecd99509f2ef73e986ae278f0c793e0bda
Eon-ts <aeonsitux@gmail.com>
2025-03-27 18:47:11 -0600

    Agregar archivos de proyecto.

---
+++ L01P022022JH650/Models/db_notasContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace L01P022022JH650.Models
+{
+    public class db_notasContext : DbContext
+    {
+        public db_notasContext(DbContextOptions options) : base(options) { }
+
+
+    }
+}
---
+++ L01P022022JH650/Models/facultades.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P022022JH650.Models
+{
+    public class facultades
+    {
+        [Key]
+        public int id { get; set; }
+        public string facultad { get; set; }
+
+        //(id       INT IDENTITY(1, 1) PRIMARY KEY, facultad VARCHAR(100)
+    }
+}
